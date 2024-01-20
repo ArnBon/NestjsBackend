08bbe88eaf62245aa50beac22a55b9d891b9e546
ArnBon <bonillaarnaldo@gmail.com>
2024-01-20 19:12:33 -0400

    clase 342 crear modelo y esquema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    AuthModule,    
+    AuthModule,
+    ConfigModule.forRoot(),    
     MongooseModule.forRoot( process.env.MONGO_URI ),
   ],
   controllers: [],
