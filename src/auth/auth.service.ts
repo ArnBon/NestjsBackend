00b6b9b944d3e9d11fec8f51482e751a602463aa
ArnBon <bonillaarnaldo@gmail.com>
2024-01-21 16:52:36 -0400

    clase 345 encriptar clave

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,6 +4,8 @@ import { UpdateAuthDto } from './dto/update-auth.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './entities/user.entity';
 import { Model } from 'mongoose';
+import * as bcryptjs from 'bcryptjs';
+
 
 @Injectable()
 export class AuthService {
@@ -16,8 +18,14 @@ export class AuthService {
   async create(createUserDto: CreateUserDto) {
 
     try {
-      const newUser = new this.userModel(createUserDto);
-      return await newUser.save();      
+      const {password, ...userData} = createUserDto;
+      const newUser = new this.userModel({
+        password:bcryptjs.hashSync(password,10),
+        ...userData  
+      });
+       await newUser.save();
+       const { password:_, ...user } = newUser.toJSON();
+       return user;    
     } catch (error) {
       if (error.code === 11000) {        
         throw new BadRequestException(`${ createUserDto.email } Ya existe!`)        
