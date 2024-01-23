bfc2bf812bd97836fa73c1f60f015d5cc771a298
ArnBon <bonillaarnaldo@gmail.com>
2024-01-23 19:22:20 -0400

    clase 352 solucion tarea

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,8 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Request }
 import { AuthService } from './auth.service';
 import { CreateUserDto, LoginDto, RegisterUserDto, UpdateAuthDto } from './dto';
 import { AuthGuard } from './guards/auth.guard';
+import { LoginResponse } from './interfaces/login-response';
+import { User } from './entities/user.entity';
 
 
 @Controller('auth')
@@ -31,19 +33,33 @@ export class AuthController {
     return this.authService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.authService.findOne(+id);
-  }
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.authService.findOne(+id);
+  // }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-    return this.authService.update(+id, updateAuthDto);
-  }
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
+  //   return this.authService.update(+id, updateAuthDto);
+  // }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.authService.remove(+id);
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.authService.remove(+id);
+  // }
+
+  // LoginResponse
+  @UseGuards( AuthGuard )
+  @Get('check-token')
+  checkToken( @Request() req: Request ): LoginResponse {
+      
+    const user = req['user'] as User;
+
+    return {
+      user,
+      token: this.authService.getJwtToken({ id: user._id })
+    }
+
   }
 
   
